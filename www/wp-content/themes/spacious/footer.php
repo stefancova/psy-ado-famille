5901a4a5adeaab397734bac144e38ea5b987e07e
stecov <stefan.cova@publicis-modem.fr>
2014-10-27 18:14:40 +0100

    add images + pages + footer gmaps

--- www/wp-content/themes/spacious/footer.php
+++ www/wp-content/themes/spacious/footer.php
@@ -14,7 +14,28 @@
 	</div><!-- #main -->	
 	<?php do_action( 'spacious_before_footer' ); ?>
 		<footer id="colophon" class="clearfix">	
-			<?php get_sidebar( 'footer' ); ?>	
+			<?php get_sidebar( 'footer' ); ?>
+
+			<div class="footer-widgets-wrapper footer-gmaps">
+				<div class="inner-wrap">
+					<div class="footer-widgets-area clearfix">
+						<div class="tg-one-third footer-perso-info">
+							<h3 class="widget-title">
+								<span>Contact rapide</span>
+							</h3>
+							<p>Cécile Martin</p>
+							<p>29 rue de Belleville</p>
+							<p>75019, Paris</p>
+						</div>
+						<div class="footer-perso-maps">
+							<iframe class="" width="100%" height="200" frameborder="0" style="border:0"
+								src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBiF1h5ZyQZUj4ECHCbzHa3A3n1V-ny8eA&q=29+rue+de+Belleville,75019+Paris">
+							</iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			<div class="footer-socket-wrapper clearfix">
 				<div class="inner-wrap">
 					<div class="footer-socket-area">
