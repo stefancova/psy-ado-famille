faa2824bc415969e6f29fd663ebd5e8363cd5f6e
stecov <stefan.cova@publicis-modem.fr>
2014-12-15 17:40:43 +0100

    #menu-perso

--- www/wp-content/themes/spacious/sidebar.php
+++ www/wp-content/themes/spacious/sidebar.php
@@ -19,28 +19,41 @@
 			}
 		?>
 
+		<div id="menu-perso">
+			<?php
+				$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+				if ($children) {
+					$parent_title = get_the_title($post->post_parent);
+					echo '<p class="parent-page">'.$parent_title.'<p>';
+				?>
+				<ul>
+					<?php echo $children; ?>
+				</ul>
+			<?php } ?>
+		</div><!-- fin menu-perso -->
+
 		<?php if ( ! dynamic_sidebar( $sidebar ) ) : ?>
 
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
+		<aside id="search" class="widget widget_search">
+			<?php get_search_form(); ?>
+		</aside>
 
-			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'spacious' ); ?></h1>
-				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-				</ul>
-			</aside>
+		<aside id="archives" class="widget">
+			<h1 class="widget-title"><?php _e( 'Archives', 'spacious' ); ?></h1>
+			<ul>
+				<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+			</ul>
+		</aside>
 
-			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'spacious' ); ?></h1>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</aside>
+		<aside id="meta" class="widget">
+			<h1 class="widget-title"><?php _e( 'Meta', 'spacious' ); ?></h1>
+			<ul>
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<?php wp_meta(); ?>
+			</ul>
+		</aside>
 
-		<?php endif; ?>
+	<?php endif; ?>
 	<?php do_action( 'spacious_after_sidebar' ); ?>
 </div>
